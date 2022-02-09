68c8b837405d875dba1d3e752574b24f55912352
liviu-lesan <liviu.lesan159@gmail.com>
2022-02-09 10:43:27 +0200

    added possibility to delete spaces

--- planetmint/backend/tarantool/database.py
+++ planetmint/backend/tarantool/database.py
@@ -13,11 +13,22 @@ class TarantoolDB:
 
 
 def init_tarantool():
-    path = os.getcwd()
-    run(["mkdir" , "tarantool"])
-    run(["ln","-s",path +"/init.lua","init.lua"] , path+"/tarantool")
-    run (["tarantool" , "init.lua"] ,path+ "/tarantool")
+    if os.path.exists(os.path.join(os.getcwd(), 'tarantool', 'init.lua')) is not True:
+        path = os.getcwd()
+        run(["mkdir" , "tarantool_snap"])
+        run(["ln","-s",path +"/init.lua","init.lua"] , path+"/tarantool_snap")
+        run (["tarantool" , "init.lua"] ,path+ "/tarantool")
+    else:
+        raise Exception("There is a instance of tarantool already created in %s" + os.getcwd() + "/tarantool_snap")
+    
+
+         
+        
 
 def drop_tarantool():
-    #TODO drop tarantool
-    pass
+    if os.path.exists(os.path.join(os.getcwd(), 'tarantool', 'init.lua')) is not True:
+        path = os.getcwd()
+        run(["ln","-s",path +"/drop_db.lua","drop_db.lua"] , path+"/tarantool_snap")
+        run (["tarantool" , "drop_db.lua"])
+    else:
+        raise Exception("There is no tarantool spaces to drop")
---
+++ planetmint/backend/tarantool/drop_db.lua
@@ -0,0 +1,5 @@
+box.space.transactions.drop()
+box.space.output.drop()
+box.space.inputs.drop()
+box.space.keys.drop()
+box.snapshot()
