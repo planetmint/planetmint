54c04b386752e387b3570cf9b79fdb11dabf2e96
Lorenz Herzberger <lorenzherzberger@gmail.com>
2022-02-17 08:52:29 +0100

    added ci secret to test script
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-result=$(ssh root@64.225.106.52 'bash -s' < ./election.sh elect 35)
-ssh root@64.225.105.60 'bash -s' < ./election.sh approve $result
+result=$(ssh root@64.225.106.52 -i id_ed25519 'bash -s' < ./election.sh elect 35)
+ssh root@64.225.105.60 -i id_ed25519 'bash -s' < ./election.sh approve $result
 
 exitcode=$?
 
