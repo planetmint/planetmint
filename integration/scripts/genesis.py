dda9914e04a6cb296a5f379b17f9f0e9750d4605
Lorenz Herzberger <lorenzherzberger@gmail.com>
2022-02-28 19:25:36 +0100

    added ipdb copyright notice
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- integration/scripts/genesis.py
+++ integration/scripts/genesis.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+# Copyright © 2020 Interplanetary Database Association e.V.,
+# Planetmint and IPDB software contributors.
+# SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+# Code is Apache-2.0 and docs are CC-BY-4.0
+
 import json
 import os
 
