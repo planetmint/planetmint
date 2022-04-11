010e4c5d147f2a4684ddd368208b6d900dd1351d
Lorenz Herzberger <lorenzherzberger@gmail.com>
2022-04-11 12:54:03 +0200

    fixed some test cases
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- tests/assets/test_digital_assets.py
+++ tests/assets/test_digital_assets.py
@@ -10,13 +10,13 @@ from planetmint.transactions.types.assets.transfer import Transfer
 
 def test_asset_transfer(b, signed_create_tx, user_pk, user_sk):
     tx_transfer = Transfer.generate(signed_create_tx.to_inputs(), [([user_pk], 1)],
-                                       signed_create_tx.id)
+                                       [signed_create_tx.id])
     tx_transfer_signed = tx_transfer.sign([user_sk])
 
     b.store_bulk_transactions([signed_create_tx])
 
     assert tx_transfer_signed.validate(b) == tx_transfer_signed
-    assert tx_transfer_signed.asset['id'] == signed_create_tx.id
+    assert tx_transfer_signed.assets[0]['id'] == signed_create_tx.id
 
 
 def test_validate_transfer_asset_id_mismatch(b, signed_create_tx, user_pk, user_sk):
@@ -24,7 +24,7 @@ def test_validate_transfer_asset_id_mismatch(b, signed_create_tx, user_pk, user_
 
     tx_transfer = Transfer.generate(signed_create_tx.to_inputs(), [([user_pk], 1)],
                                        [signed_create_tx.id])
-    tx_transfer.asset['id'] = 'a' * 64
+    tx_transfer.assets[0]['id'] = 'a' * 64
     tx_transfer_signed = tx_transfer.sign([user_sk])
 
     b.store_bulk_transactions([signed_create_tx])
@@ -36,7 +36,7 @@ def test_validate_transfer_asset_id_mismatch(b, signed_create_tx, user_pk, user_
 def test_get_asset_id_create_transaction(alice, user_pk):
     from planetmint.models import Transaction
     tx_create = Create.generate([alice.public_key], [([user_pk], 1)])
-    assert Transaction.get_asset_ids(tx_create) == tx_create.id
+    assert Transaction.get_asset_ids(tx_create)[0] == tx_create.id
 
 
 def test_get_asset_id_transfer_transaction(b, signed_create_tx, user_pk):
@@ -44,23 +44,9 @@ def test_get_asset_id_transfer_transaction(b, signed_create_tx, user_pk):
 
     tx_transfer = Transfer.generate(signed_create_tx.to_inputs(), [([user_pk], 1)],
                                        [signed_create_tx.id])
-    asset_id = Transaction.get_asset_ids(tx_transfer)
-    assert asset_id == tx_transfer.asset['id']
+    asset_ids = Transaction.get_asset_ids(tx_transfer)
+    assert asset_ids[0] == tx_transfer.assets[0]['id']
 
-# This test is not relevant anymore
-def test_asset_id_mismatch(alice, user_pk):
-    from planetmint.models import Transaction
-    from planetmint.transactions.common.exceptions import AssetIdMismatch
-
-    tx1 = Create.generate([alice.public_key], [([user_pk], 1)],
-                             metadata={'msg': random.random()})
-    tx1.sign([alice.private_key])
-    tx2 = Create.generate([alice.public_key], [([user_pk], 1)],
-                             metadata={'msg': random.random()})
-    tx2.sign([alice.private_key])
-
-    with pytest.raises(AssetIdMismatch):
-        Transaction.get_asset_ids([tx1, tx2])
 
 
 def test_create_valid_divisible_asset(b, user_pk, user_sk):
