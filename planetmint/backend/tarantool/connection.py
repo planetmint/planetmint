b6264d518c0e703b44ce2091ab365a560c75ee0f
andrei <deffuls@debian-BULLSEYE-live-builder-AMD64>
2022-04-12 18:06:20 +0300

    test_schema.py ALL PASSING + changes in TarantoolDB Class

--- planetmint/backend/tarantool/connection.py
+++ planetmint/backend/tarantool/connection.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class TarantoolDB:
-    def __init__(self, host: str = None, port: int = None, user: str = None, password: str = None,
+    def __init__(self, host: str = "localhost", port: int = 3303, user: str = None, password: str = None,
                  reset_database: bool = False):
         self.host = host
         self.port = port
@@ -24,30 +24,33 @@ class TarantoolDB:
         self.db_connect = tarantool.connect(host=self.host, port=self.port)
         self.init_path = Config().get()["database"]["init_config"]["absolute_path"]
         self.drop_path = Config().get()["database"]["drop_config"]["absolute_path"]
-        if reset_database:
-            self.drop_database()
-            self.init_database()
+        # if reset_database:
+        #     self.drop_database()
+        #     self.init_database()
         self.SPACE_NAMES = ["abci_chains", "assets", "blocks", "blocks_tx",
                             "elections", "meta_data", "pre_commits", "validators",
                             "transactions", "inputs", "outputs", "keys"]
 
+    def _reconnect(self):
+        self.db_connect = tarantool.connect(host=self.host, port=self.port)
+
     def space(self, space_name: str):
-        try:
-            return self.db_connect.space(space_name)
-        except tarantool.error.SchemaError:
-            return None
+        return self.db_connect.space(space_name)
 
     def get_connection(self):
         return self.db_connect
 
     def drop_database(self):
         db_config = Config().get()["database"]
-        return self.run_command(command=self.drop_path, config=db_config)
+        cmd_resp = self.run_command(command=self.drop_path, config=db_config)
+        self._reconnect()
+        return cmd_resp
 
     def init_database(self):
         db_config = Config().get()["database"]
-
-        return self.run_command(command=self.init_path, config=db_config)
+        cmd_resp = self.run_command(command=self.init_path, config=db_config)
+        self._reconnect()
+        return cmd_resp
 
     def run_command(self, command: str, config: dict):
         import subprocess
@@ -57,7 +60,7 @@ class TarantoolDB:
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             universal_newlines=True,
-            bufsize=0,
+            bufsize=1,
             shell=True).stdout.readlines()
         # TODO verify if subprocess creation worked properly
         return True if "nil value" not in ret else False
--- tests/backend/tarantool/test_schema.py
+++ tests/backend/tarantool/test_schema.py
@@ -3,16 +3,8 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-from planetmint.config import Config
-from planetmint.backend import Connection
 from planetmint.backend.tarantool.connection import TarantoolDB
 
-
-# This function creates database, we have now only spaces so for now it is commented
-# def test_init_database_is_graceful_if_db_exists():
-#     conn = TarantoolDB('localhost', 3303)
-#     conn.drop_database()
-#     conn.init_database()
 
 def _check_spaces_by_list(conn, space_names):
     _exists = []
@@ -26,16 +18,15 @@ def _check_spaces_by_list(conn, space_names):
 
 
 def test_create_tables():
-    conn = TarantoolDB('localhost', 3303)
-    # The db is set up by the fixtures so we need to remove it
-    # conn.drop_database()
-    conn.init_database()
-
-    assert conn.SPACE_NAMES == _check_spaces_by_list(conn=conn, space_names=conn.SPACE_NAMES)
+    db_conn = TarantoolDB("localhost", 3303)
+    db_conn.drop_database()
+    db_conn.init_database()
+    assert db_conn.SPACE_NAMES == _check_spaces_by_list(conn=db_conn, space_names=db_conn.SPACE_NAMES)
 
 
 def test_drop():  # remove dummy_db as argument
-    conn = TarantoolDB('localhost', 3303)
-    conn.drop_database()
-    actual_spaces = _check_spaces_by_list(conn=conn, space_names=conn.SPACE_NAMES)
+    db_conn = TarantoolDB("localhost", 3303)
+    db_conn.drop_database()
+    actual_spaces = _check_spaces_by_list(conn=db_conn, space_names=db_conn.SPACE_NAMES)
     assert [] == actual_spaces
+
