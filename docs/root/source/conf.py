af681f763f3297972992e53c8d0eb710ca0b1a04
Jürgen Eckel <juergen@riddleandcode.com>
2022-06-07 23:45:32 +0200

    changed to the press theme
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- docs/root/source/conf.py
+++ docs/root/source/conf.py
@@ -185,7 +185,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'press'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -194,7 +194,7 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [press.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
