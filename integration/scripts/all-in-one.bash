6a6ec8ea6a15716a3576b306a5d3571578a8f246
Lorenz Herzberger <lorenzherzberger@gmail.com>
2022-06-30 10:39:50 +0200

    updated integration test setup for tarantool
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- integration/scripts/all-in-one.bash
+++ integration/scripts/all-in-one.bash
@@ -4,18 +4,11 @@
 # SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 # Code is Apache-2.0 and docs are CC-BY-4.0
 
-
-# MongoDB configuration
-# [ "$(stat -c %U /data/db)" = mongodb ] || chown -R mongodb /data/db
-# Tarantool configuration
-echo STARTING TARANTOOL NOW
-tarantool /usr/src/app/scripts/init.lua
-
 # Planetmint configuration
 /usr/src/app/scripts/planetmint-monit-config
 
-# nohup mongod --bind_ip_all > "$HOME/.planetmint-monit/logs/mongodb_log_$(date +%Y%m%d_%H%M%S)" 2>&1 &
-
+# Tarantool startup and configuration
+tarantool /usr/src/app/scripts/init.lua
 
 # Start services
 monit -d 5 -I -B
--- integration/scripts/init.lua
+++ integration/scripts/init.lua
@@ -1,85 +1,86 @@
 #!/usr/bin/env tarantool
 box.cfg {
-    listen = 3303,
-    background = true,
-    log = '1.log',
-    pid_file = '1.pid',
-    log_level = 7
+  listen = 3303,
+  background = true,
+  log = '.planetmint-monit/logs/tarantool.log',
+  pid_file = '.planetmint-monit/monit_processes/tarantool.pid'
 }
 
-box.schema.user.create('test', { password = 'test' })
-box.schema.user.grant('test', 'execute,read,write', 'universe')
+box.schema.user.grant('guest','read,write,execute,create,drop','universe')
 
-box.once("bootstrap", function()
-    abci_chains = box.schema.space.create('abci_chains', {engine='memtx', is_sync = false})
-    abci_chains:format({{name='height' , type='integer'},{name='is_synched' , type='boolean'},{name='chain_id',type='string'}})
-    abci_chains:create_index('id_search' ,{type='hash', parts={'chain_id'}})
-    abci_chains:create_index('height_search' ,{type='tree',unique=false, parts={'height'}})
-    
-    assets = box.schema.space.create('assets' , {engine='memtx' , is_sync=false})
-    assets:format({{name='data' , type='any'}, {name='tx_id', type='string'}, {name='asset_id', type='string'}})
-    assets:create_index('txid_search', {type='hash', parts={'tx_id'}})
-    assets:create_index('assetid_search', {type='tree',unique=false, parts={'asset_id', 'tx_id'}})
-    assets:create_index('only_asset_search', {type='tree', unique=false, parts={'asset_id'}})
-    
-    blocks = box.schema.space.create('blocks' , {engine='memtx' , is_sync=false})
-    blocks:format({{name='app_hash',type='string'},{name='height' , type='integer'},{name='block_id' , type='string'}})
-    blocks:create_index('id_search' , {type='hash' , parts={'block_id'}})
-    blocks:create_index('block_search' , {type='tree', unique = false, parts={'height'}})
-    blocks:create_index('block_id_search', {type = 'hash', parts ={'block_id'}})
-    
-    blocks_tx = box.schema.space.create('blocks_tx')
-    blocks_tx:format({{name='transaction_id', type = 'string'}, {name = 'block_id', type = 'string'}})
-    blocks_tx:create_index('id_search',{ type = 'hash', parts={'transaction_id'}})
-    blocks_tx:create_index('block_search', {type = 'tree',unique=false, parts={'block_id'}})
-    
-    elections = box.schema.space.create('elections',{engine = 'memtx' , is_sync = false})
-    elections:format({{name='election_id' , type='string'},{name='height' , type='integer'}, {name='is_concluded' , type='boolean'}})
-    elections:create_index('id_search' , {type='hash', parts={'election_id'}})
-    elections:create_index('height_search' , {type='tree',unique=false, parts={'height'}})
-    elections:create_index('update_search', {type='tree', unique=false, parts={'election_id', 'height'}})
-    
-    meta_datas = box.schema.space.create('meta_data',{engine = 'memtx' , is_sync = false})
-    meta_datas:format({{name='transaction_id' , type='string'}, {name='meta_data' , type='any'}})
-    meta_datas:create_index('id_search', { type='hash' , parts={'transaction_id'}})
-    
-    pre_commits = box.schema.space.create('pre_commits' , {engine='memtx' , is_sync=false})
-    pre_commits:format({{name='commit_id', type='string'}, {name='height',type='integer'}, {name='transactions',type=any}})
-    pre_commits:create_index('id_search', {type ='hash' , parts={'commit_id'}})
-    pre_commits:create_index('height_search', {type ='tree',unique=true, parts={'height'}})
-    
-    validators = box.schema.space.create('validators' , {engine = 'memtx' , is_sync = false})
-    validators:format({{name='validator_id' , type='string'},{name='height',type='integer'},{name='validators' , type='any'}})
-    validators:create_index('id_search' , {type='hash' , parts={'validator_id'}})
-    validators:create_index('height_search' , {type='tree', unique=true, parts={'height'}})
-    
-    transactions = box.schema.space.create('transactions',{engine='memtx' , is_sync=false})
-    transactions:format({{name='transaction_id' , type='string'}, {name='operation' , type='string'}, {name='version' ,type='string'}, {name='dict_map', type='any'}})
-    transactions:create_index('id_search' , {type = 'hash' , parts={'transaction_id'}})
-    transactions:create_index('transaction_search' , {type = 'tree',unique=false, parts={'operation', 'transaction_id'}})
-    
-    inputs = box.schema.space.create('inputs')
-    inputs:format({{name='transaction_id' , type='string'}, {name='fulfillment' , type='any'}, {name='owners_before' , type='array'}, {name='fulfills_transaction_id', type = 'string'}, {name='fulfills_output_index', type = 'string'}, {name='input_id', type='string'}, {name='input_index', type='number'}})
-    inputs:create_index('delete_search' , {type = 'hash', parts={'input_id'}})
-    inputs:create_index('spent_search' , {type = 'tree', unique=false, parts={'fulfills_transaction_id', 'fulfills_output_index'}})
-    inputs:create_index('id_search', {type = 'tree', unique=false, parts = {'transaction_id'}})
-    
-    outputs = box.schema.space.create('outputs')
-    outputs:format({{name='transaction_id' , type='string'}, {name='amount' , type='string'}, {name='uri', type='string'}, {name='details_type', type='string'}, {name='details_public_key', type='any'}, {name = 'output_id', type = 'string'}, {name='treshold', type='any'}, {name='subconditions', type='any'}, {name='output_index', type='number'}})
-    outputs:create_index('unique_search' ,{type='hash', parts={'output_id'}})
-    outputs:create_index('id_search' ,{type='tree', unique=false, parts={'transaction_id'}})
-    
-    keys = box.schema.space.create('keys')
-    keys:format({{name = 'id', type='string'}, {name = 'transaction_id', type = 'string'} ,{name = 'output_id', type = 'string'}, {name = 'public_key', type = 'string'}, {name = 'key_index', type = 'integer'}})
-    keys:create_index('id_search', {type = 'hash', parts={'id'}})
-    keys:create_index('keys_search', {type = 'tree', unique=false, parts={'public_key'}})
-    keys:create_index('txid_search', {type = 'tree', unique=false, parts={'transaction_id'}})
-    keys:create_index('output_search', {type = 'tree', unique=false, parts={'output_id'}})
-    
-    utxos = box.schema.space.create('utxos', {engine = 'memtx' , is_sync = false})
-    utxos:format({{name='transaction_id' , type='string'}, {name='output_index' , type='integer'}, {name='utxo_dict', type='string'}})
-    utxos:create_index('id_search', {type='hash' , parts={'transaction_id', 'output_index'}})
-    utxos:create_index('transaction_search', {type='tree', unique=false, parts={'transaction_id'}})
-    utxos:create_index('index_search', {type='tree', unique=false, parts={'output_index'}})    
-end)
-
+function indexed_pattern_search(space_name, field_no, pattern)
+  if (box.space[space_name] == nil) then
+    print("Error: Failed to find the specified space")
+    return nil
+  end
+  local index_no = -1
+  for i=0,box.schema.INDEX_MAX,1 do
+    if (box.space[space_name].index[i] == nil) then break end
+    if (box.space[space_name].index[i].type == "TREE"
+        and box.space[space_name].index[i].parts[1].fieldno == field_no
+        and (box.space[space_name].index[i].parts[1].type == "scalar"
+        or box.space[space_name].index[i].parts[1].type == "string")) then
+      index_no = i
+      break
+    end
+  end
+  if (index_no == -1) then
+    print("Error: Failed to find an appropriate index")
+    return nil
+  end
+  local index_search_key = ""
+  local index_search_key_length = 0
+  local last_character = ""
+  local c = ""
+  local c2 = ""
+  for i=1,string.len(pattern),1 do
+    c = string.sub(pattern, i, i)
+    if (last_character ~= "%") then
+      if (c == '^' or c == "$" or c == "(" or c == ")" or c == "."
+                    or c == "[" or c == "]" or c == "*" or c == "+"
+                    or c == "-" or c == "?") then
+        break
+      end
+      if (c == "%") then
+        c2 = string.sub(pattern, i + 1, i + 1)
+        if (string.match(c2, "%p") == nil) then break end
+        index_search_key = index_search_key .. c2
+      else
+        index_search_key = index_search_key .. c
+      end
+    end
+    last_character = c
+  end
+  index_search_key_length = string.len(index_search_key)
+  local result_set = {}
+  local number_of_tuples_in_result_set = 0
+  local previous_tuple_field = ""
+  while true do
+    local number_of_tuples_since_last_yield = 0
+    local is_time_for_a_yield = false
+    for _,tuple in box.space[space_name].index[index_no]:
+    pairs(index_search_key,{iterator = box.index.GE}) do
+      if (string.sub(tuple[field_no], 1, index_search_key_length)
+      > index_search_key) then
+        break
+      end
+      number_of_tuples_since_last_yield = number_of_tuples_since_last_yield + 1
+      if (number_of_tuples_since_last_yield >= 10
+          and tuple[field_no] ~= previous_tuple_field) then
+        index_search_key = tuple[field_no]
+        is_time_for_a_yield = true
+        break
+        end
+      previous_tuple_field = tuple[field_no]
+      if (string.match(tuple[field_no], pattern) ~= nil) then
+        number_of_tuples_in_result_set = number_of_tuples_in_result_set + 1
+        result_set[number_of_tuples_in_result_set] = tuple
+      end
+    end
+    if (is_time_for_a_yield ~= true) then
+      break
+    end
+    require('fiber').yield()
+  end
+  return result_set
+end
