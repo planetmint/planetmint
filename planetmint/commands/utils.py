0c2d4487cbfc684d75163117718c0c2d0a9ccba9
Jürgen Eckel <juergen@riddleandcode.com>
2023-01-25 10:27:54 +0100

    fixed testcaes issue with patching

--- planetmint/commands/utils.py
+++ planetmint/commands/utils.py
@@ -10,7 +10,7 @@ for ``argparse.ArgumentParser``.
 import argparse
 import builtins
 import functools
-from multiprocessing import cpu_count
+import multiprocessing
 import sys
 import planetmint
 import planetmint.config_utils
@@ -132,7 +132,7 @@ def start(parser, argv, scope):
     if args.multiprocess is False:
         args.multiprocess = 1
     elif args.multiprocess is None:
-        args.multiprocess = cpu_count()
+        args.multiprocess = multiprocessing.cpu_count()
 
     return func(args)
 
