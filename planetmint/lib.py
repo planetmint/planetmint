faeb0ee41947d778b606f3709f390399eb9d46f1
Jürgen Eckel <juergen@riddleandcode.com>
2023-02-21 10:09:33 +0100

    blackified
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- planetmint/lib.py
+++ planetmint/lib.py
@@ -918,7 +918,7 @@ class Planetmint(object):
         self.delete_elections(new_height)
 
         txns = [self.get_transaction(tx_id) for tx_id in txn_ids]
-        
+
         txns = [Transaction.from_dict(tx.to_dict()) for tx in txns if tx]
 
         elections = self._get_votes(txns)
